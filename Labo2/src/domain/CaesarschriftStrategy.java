a85a339038e01dccad844a15ca3f6c664b13c7c1
akselele <axelelexd@gmail.com>
2019-09-26 14:27:33 +0200

    Merge branch 'master' of C:\Users\axlut\Desktop\TI3\OefeningenOOO with conflicts.

--- Labo2/src/domain/CaesarschriftStrategy.java
+++ Labo2/src/domain/CaesarschriftStrategy.java
@@ -1,9 +1,23 @@
 package domain;
 
 public class CaesarschriftStrategy implements GeheimschriftStrategy{
+    String alfabet ="abcdefghijklmnopqrstuvwxyz";
+    String uitkomst;
 
     @Override
     public String algoritme(String text) {
-        return null;
+        text = text.toLowerCase();
+        for(int i =1; i < text.length(); i++){
+            for(int x = 0; x < alfabet.length(); x++){
+                if(text.charAt(i) == alfabet.charAt(x)){
+                    if(x>3){
+                        x = x + 23 - 26;
+                    }
+                    uitkomst += alfabet.charAt(x);
+                }
+            }
+        }
+        System.out.println(uitkomst);
+        return uitkomst;
     }
 }
