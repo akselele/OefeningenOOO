5d72feba4c940b124ab91ebdd4b871f92481ceca
akselele <axelelexd@gmail.com>
2019-10-10 14:17:45 +0200

    Statusstate interface defaults

--- Labo3/src/domain/StatusState.java
+++ Labo3/src/domain/StatusState.java
@@ -4,8 +4,16 @@ package domain;
 public interface StatusState {
 
     //throw exceptions as standard
-    void verwijderen();
-    void uitlenen();
-    void herstellen();
-    double terugbrengen();
+    default void verwijderen(){
+        throw new IllegalArgumentException("Verwijderen gaat niet");
+    }
+    default void uitlenen(){
+        throw new IllegalArgumentException("Uitlenen gaat niet");
+    }
+    default void herstellen(){
+        throw new IllegalArgumentException("Herstellen gaat niet");
+    }
+    default void terugbrengen(){
+        throw new IllegalArgumentException("Terugbrengen gaat niet");
+    }
 }
