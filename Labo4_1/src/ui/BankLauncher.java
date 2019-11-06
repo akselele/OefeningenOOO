09eb2fe09d77936667969f9436b3357f67bd645b
Kasper Vanderhulst <kasper.vanderhulst@student.ucll.be>
2019-11-06 11:53:24 +0100

    changed name

--- Labo4_1/src/ui/BankLauncher.java
+++ Labo4_1/src/ui/BankLauncher.java
@@ -5,8 +5,7 @@ import domain.Bank;
 
 public class BankLauncher {
     public static void main(String[] args) {
-        Bank bank = new Bank();
-        Account account = new Account(1200,"BE48621188");
-        bank.openAccount(account);
+        Bank bank = new Bank("KBC");
+        bank.openAccount(2500);
     }
 }
